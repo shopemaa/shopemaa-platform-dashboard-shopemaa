7b338096363c45462d92d21be29cbd7270cdd59a
Abid <fmaabid@gmail.com>
2025-10-16 23:30:58 +0600

    table

--- components/categories/CategoryList.jsx
+++ components/categories/CategoryList.jsx
@@ -49,6 +49,72 @@ export default function CategoryList() {
           )}
         </div>
       </div>
+
+      {/* Table */}
+      <div className="table-responsive">
+        <table className="table table-vcenter card-table qrcentral-table">
+          <thead className="d-md-table-header-group">
+            <tr>
+              <th style={{ width: 36 }}>
+                <label className="form-check m-0">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    aria-label="Select all products"
+                  />
+                </label>
+              </th>
+              <th>Category</th>
+              <th className="w-1 text-end">Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td data-header="">
+                <div className="form-check m-0">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    aria-label={`Select `}
+                  />
+                </div>
+              </td>
+              <td data-header="SKU">
+                <div className="text-muted">Category 1</div>
+              </td>
+
+              <td data-header="SKU">
+                <div className="text-muted">
+                  <button
+                    className="btn btn-sm btn-outline"
+                    title="Quick view"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#productQuickView"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 24 24"
+                      strokeWidth="2"
+                      stroke="currentColor"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" />
+                      <circle cx="12" cy="12" r="2" />
+                      <path d="M22 12c-2.667 4 -6 6 -10 6s-7.333 -2 -10 -6c2.667 -4 6 -6 10 -6s7.333 2 10 6" />
+                    </svg>
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
